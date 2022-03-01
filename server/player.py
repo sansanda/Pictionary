45192205a986e8f85e19bf625b1b58a3ca5d7f22
DavidS <dsanchezsanc@uoc.edu>
2022-03-01 15:28:26 +0100

    created Player.py on server

---
+++ server/player.py
@@ -0,0 +1,26 @@
+"""
+Repreents a Player Object on the server side
+"""
+
+class Player(object):
+
+    def __init__(self, ip, name):
+        self.ip = ip
+        self.name = name
+        self.score = 0
+
+    def update_score (self, x):
+        self.score += x
+    def guess(self, string):
+        pass
+    def disconnect(self, string):
+        pass
+
+    def get_ip(self):
+        return self.ip
+    def get_name(self):
+        return self.name
+    def get_score(self):
+        return self.score
+
+
